f6f8400c1e8fe46d3a530f970fa1c1601c9541b5
DJ Picard <dj.picard@onepeloton.com>
2021-08-25 14:43:19 -0400

    Minor updates

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ locals {
 variable "inputs" {
   description = "REQUIRED = Map of inputs for this specific account"
   type        = string
-  default     = "{name:ample\",\"region\":\"us-east-1\"}"
+  default     = "{name:sample\",\"region\":\"us-east-1\"}"
 }
 
 output "name" {
@@ -32,4 +32,8 @@ output "account_tags" {
 
 output "enable_flow_log" {
   value = local.enable_flow_log
+}
+
+output "private_subnet_tags" {
+  value = local.private_subnet_tags
 }
